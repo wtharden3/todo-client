b2fe3c1c43e35101627de123b3999118f458787f
Whitney Harden <wtharden320@gmail.com>
2020-11-25 11:40:19 -0500

    Navbar edits to come. Commented out

--- src/Components/home/Navbar.jsx
+++ src/Components/home/Navbar.jsx
@@ -1,12 +1,37 @@
+import React, {useState, useEffect} from 'react';
 import{Nav, NavItem, Button} from 'reactstrap';
 
 const Navbar = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [onLoginPage, setOnLoginPage] = useState(true)
+  const [signUpSuccessful, setSignUpSuccessful] = useState(false);
   //const [token, setToken] = useState()
   const logOut = () => {
     props.setToken(undefined);
   }
 
   return(
+    // <div className="col-3">
+    //   <p>Vertical Nav</p>
+    //   <Nav vertical>
+    //     <NavItem>
+    //       NavItem 1
+    //     </NavItem>
+    //     <NavItem>
+    //       NavItem 1
+    //     </NavItem>
+    //     <NavItem>
+    //       NavItem 1
+    //     </NavItem>
+    //     <NavItem>
+    //       NavItem 1
+    //     </NavItem>
+    //     <NavItem>
+    //       <Button onClick={signUpToggle}>{onLoginPage  ? 'Switch to Signup ' : 'Switch to Login'}</Button>
+    //     </NavItem>
+    //   </Nav>
+    // </div>
+    //two different navs 1 with login signup toggle that will
     <div className="col-3">
       <p>Vertical Nav</p>
       <Nav vertical>
