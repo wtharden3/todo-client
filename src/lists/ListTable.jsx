3d37d8ad7e9e9ecb0c7eac6cc71944f483b7106a
Whitney Harden <wtharden320@gmail.com>
2020-11-30 12:44:51 -0500

    sort feature added and updated input types for dates

--- src/lists/ListTable.jsx
+++ src/lists/ListTable.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Table, Button } from 'reactstrap';
 
 const ListTable = props => {
+  const [durationSorted, setDurationSorted] = useState(false);
+  const [dateSorted, setDateSorted] = useState(false);
+  const [taskNameSorted, setTaskNameSorted] = useState(false);
+  const [dueDateSorted, setDueDateSorted] = useState(false);
+
   const deleteList = list => {
     fetch(`http://localhost:4000/lists/delete/${list.id}`, {
       method: 'DELETE',
@@ -12,14 +17,176 @@ const ListTable = props => {
     }).then(() => props.fetchLists());
   };
 
+  const compareDurationDec = (a, b) => {
+    const durationA = a.duration;
+    const durationB = b.duration;
+
+    let comparison = 0;
+    if (durationA > durationB) {
+      comparison = 1;
+    } else if (durationA < durationB) {
+      comparison = -1;
+    }
+    return comparison;
+  };
+
+  const compareDurationAsc = (a, b) => {
+    const durationA = a.duration;
+    const durationB = b.duration;
+
+    let comparison = 0;
+    if (durationA < durationB) {
+      comparison = 1;
+    } else if (durationA > durationB) {
+      comparison = -1;
+    }
+    return comparison;
+  };
+
+//sort Date
+  const compareDateDec = (a, b) => {
+    const dateA = a.date.toUpperCase();
+    const dateB = b.date.toUpperCase();
+
+    let comparison = 0;
+    if (dateA > dateB) {
+      comparison = 1;
+    } else if (dateA < dateB) {
+      comparison = -1;
+    }
+    return comparison;
+  };
+
+  const compareDateAsc = (a, b) => {
+    const dateA = a.date.toUpperCase();
+    const dateB = b.date.toUpperCase();
+
+    let comparison = 0;
+    if (dateA < dateB) {
+      comparison = 1;
+    } else if (dateA > dateB) {
+      comparison = -1;
+    }
+    return comparison;
+  };
+  
+  //sort Task Name (listName = Task Name)
+  const compareTaskNameDec = (a, b) => {
+    const taskNameA = a.listName.toUpperCase();
+    const taskNameB = b.listName.toUpperCase();
+    console.log('taskNameA', taskNameA)
+
+    let comparison = 0;
+    if (taskNameA > taskNameB) {
+      comparison = 1;
+    } else if (taskNameA < taskNameB) {
+      comparison = -1;
+    }
+    return comparison;
+  };
+
+  const compareTaskNameAsc = (a, b) => {
+    const taskNameA = a.listName.toUpperCase();
+    const taskNameB = b.listName.toUpperCase();
+    console.log('taskNameA', taskNameA)
+
+    let comparison = 0;
+    if (taskNameA < taskNameB) {
+      comparison = 1;
+    } else if (taskNameA > taskNameB) {
+      comparison = -1;
+    }
+    return comparison;
+  };
+  //sort Date
+  const compareDueDateDec = (a, b) => {
+    const dueDateA = a.timeDue.toUpperCase();
+    const dueDateB = b.timeDue.toUpperCase();
+
+    let comparison = 0;
+    if (dueDateA > dueDateB) {
+      comparison = 1;
+    } else if (dueDateA < dueDateB) {
+      comparison = -1;
+    }
+    return comparison;
+  };
+
+  const compareDueDateAsc = (a, b) => {
+    const dueDateA = a.timeDue.toUpperCase();
+    const dueDateB = b.timeDue.toUpperCase();
+
+    let comparison = 0;
+    if (dueDateA < dueDateB) {
+      comparison = 1;
+    } else if (dueDateA > dueDateB) {
+      comparison = -1;
+    }
+    return comparison;
+  };
+  
+
+  // SORT FUNCTIONS
+
+
+  const sortDurationBtn = () => {
+    if (!durationSorted) {
+      props.lists.sort(compareDurationDec);
+      setDurationSorted(true);
+    } else if (durationSorted) {
+      props.lists.sort(compareDurationAsc);
+      setDurationSorted(false);
+    }
+  };
+
+  const sortDateBtn = () => {
+    if (!dateSorted){
+      props.lists.sort(compareDateDec)
+      setDateSorted(true)
+    }else if(dateSorted){
+      props.lists.sort(compareDateAsc)
+      setDateSorted(false);
+    }
+  }
+
+  const sortTaskNameBtn = () => {
+    if (!taskNameSorted){
+      props.lists.sort(compareTaskNameDec)
+      setTaskNameSorted(true)
+    }else if(taskNameSorted){
+      props.lists.sort(compareTaskNameAsc)
+      setTaskNameSorted(false);
+    }
+  }
+
+  const sortDueDateBtn = () => {
+    if (!dueDateSorted){
+      props.lists.sort(compareDueDateDec)
+      setDueDateSorted(true)
+    }else if(dueDateSorted){
+      props.lists.sort(compareDueDateAsc)
+      setDueDateSorted(false);
+    }
+  }
+
+  
+
   const listMapper = () => {
+    console.log('props.lists', props.lists);
+
     return props.lists.map((list, index) => {
+      //sort first -- dont use useEffect yet (or ever) (need a useEffect? to listen to button)
+      //may have a parameter in our compare fn so that we can choose which one to compare
+
+      console.log(`index: ${index}, list: ${list}`);
+
+      //just display
       return (
         <tr key={index}>
           <th scope="row">{list.owner_id}</th>
           <td>{list.date}</td>
           <td>{list.listName}</td>
-          <td>{list.duration}</td>
+          <td>{list.duration} minutes</td>
           <td>{list.timeDue}</td>
           <td>{list.description}</td>
           <td>{`${list.isChecked}`}</td>
@@ -55,12 +222,20 @@ const ListTable = props => {
         <thead>
           <tr>
             <th>#</th>
-            <th>date</th>
-            <th>listName</th>
-            <th>Duration</th>
-            <th>timeDue</th>
+            <th>Date Created
+              <button onClick={sortDateBtn}>Click</button>
+            </th>
+            <th>Task Name
+              <button onClick={sortTaskNameBtn}>Click</button>
+            </th>
+            <th>
+              Duration <button onClick={sortDurationBtn}>Click</button>
+            </th>
+            <th>Due Date
+              <button onClick={sortDueDateBtn}>Click</button>
+            </th>
             <th>Description</th>
-            <th>isChecked</th>
+            <th>Completed</th>
           </tr>
         </thead>
         <tbody>{listMapper()}</tbody>
